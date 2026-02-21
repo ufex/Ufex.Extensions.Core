67857c87dc158c636e9ed71ff061896f16283c02
Eddie Potocko <epotocko@gmail.com>
2026-02-21 16:40:20 -0500

    Minor fixes in PNG file type

--- src/PNG/PngFileType.cs
+++ src/PNG/PngFileType.cs
@@ -67,7 +67,6 @@ public class PngFileType : FileType
 		ShowGraphic = true;
 		ShowTechnical = true;
 		ShowFileCheck = false;
-		Log.SetLogName("PNG.log");
 	}
 
 	public override bool ProcessFile()
